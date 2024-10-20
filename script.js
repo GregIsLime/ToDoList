5cb05061a7e114b9958449d81b465cb2c0a573ff
Greg <specture9@gmail.com>
2024-10-21 00:14:01 +0300

    12

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+
+// add the task
 let button = document.querySelector(".button")
 console.log(button);
 button.addEventListener("click", () => {
-    
     let getContent = document.querySelector(".inputTask").value
-    if(getContent!==""){
-    let tr = document.querySelector("tr")
-    let td = document.createElement("td")
-    let p = document.createElement("p")
-    p.innerText=getContent
-    let div = document.createElement("div")
-    div.className="close-task"
-    div.innerText="Close"
-    td.append(p,div)
-  
-    console.log(tr.append(td))
+    if (getContent !== "") {
+        let tr = document.querySelector("tr")
+        let td = document.createElement("td")
+        let p = document.createElement("p")
+        p.innerText = getContent
+        let div = document.createElement("div")
+        div.className = "close-task"
+        div.innerText = "Close"
+        div.addEventListener("click", () => {
+            div.parentElement.remove()
+        })
+        td.append(p, div)
+        tr.append(td)
     }
-    // tr.append(td)
-
 })
+
+// add the task END
