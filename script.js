9adf15757517e479bf898d9c477741bc10fc02b9
Greg <specture9@gmail.com>
2024-10-21 14:47:21 +0300

    123456

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
-// add the task
 let button = document.querySelector(".button")
 console.log(button);
 button.addEventListener("click", () => {
     let getContent = document.querySelector(".inputTask").value
     if (getContent !== "") {
+        document.querySelector(".inputTask").value=""
         let tr = document.querySelector("tr")
         let td = document.createElement("td")
         let p = document.createElement("p")
+       
         p.innerText = getContent
         let div = document.createElement("div")
         div.className = "close-task"
@@ -17,7 +18,15 @@ button.addEventListener("click", () => {
         })
         td.append(p, div)
         tr.append(td)
+
+
+        p.addEventListener("click", () => {
+            if(p.getAttribute("style")==="text-decoration: line-through;"){
+            p.style = "text-decoration:none;"
+            }else{p.style = "text-decoration:line-through;"}
+           console.log(p.getAttribute("style"))
+        })
     }
 })
 
-// add the task END
+
